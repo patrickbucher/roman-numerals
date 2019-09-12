792377ace79b45ab7759e3f3022549ce7c07ea87
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-12 15:55:57 +0200

    re-formatted code

--- src/test/java/com/github/patrickbucher/RomanNumeralsTest.java
+++ src/test/java/com/github/patrickbucher/RomanNumeralsTest.java
@@ -5,27 +5,27 @@ import org.junit.Test;
 
 public class RomanNumeralsTest {
 
-  @Test
-  public void testEmptyStringToNumber() {
-    final String roman = "";
-    final int number = RomanNumerals.toNumber(roman);
-    Assert.assertEquals(0, number);
-  }
-
-  @Test
-  public void testNonRomanCharacters() {
-    final String nonRoman = "MDCLXVIk";
-    try {
-      RomanNumerals.toNumber(nonRoman);
-      Assert.fail(nonRoman + " must cause an exception");
-    } catch (final IllegalArgumentException ex) {
+    @Test
+    public void testEmptyStringToNumber() {
+        final String roman = "";
+        final int number = RomanNumerals.toNumber(roman);
+        Assert.assertEquals(0, number);
     }
-  }
 
-  @Test
-  public void test1984() {
-    final String roman = "MCMLXXXIV";
-    final int number = RomanNumerals.toNumber(roman);
-    Assert.assertEquals(1984, number);
-  }
+    @Test
+    public void testNonRomanCharacters() {
+        final String nonRoman = "MDCLXVIk";
+        try {
+            RomanNumerals.toNumber(nonRoman);
+            Assert.fail(nonRoman + " must cause an exception");
+        } catch (final IllegalArgumentException ex) {
+        }
+    }
+
+    @Test
+    public void test1984() {
+        final String roman = "MCMLXXXIV";
+        final int number = RomanNumerals.toNumber(roman);
+        Assert.assertEquals(1984, number);
+    }
 }
